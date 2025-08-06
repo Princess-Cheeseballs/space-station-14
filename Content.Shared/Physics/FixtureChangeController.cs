6cfa275b70019fbbbdebbffcd207889e17435932
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-08-06 09:18:20 -0700

    Hhnnng

---
+++ Content.Shared/Physics/FixtureChangeController.cs
@@ -0,0 +1,22 @@
+﻿using Robust.Shared.Physics;
+
+namespace Content.Shared.Physics;
+
+/// <summary>
+/// This handles the changing of fixtures on an entity, so that multiple systems can remove/add fixtures without causing problems.
+/// </summary>
+public sealed class FixtureChangeController : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+
+    }
+
+    public bool TryReenableFixtures(Entity<FixturesComponent?> entity)
+    {
+
+
+        return true;
+    }
+}
